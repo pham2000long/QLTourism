b3dad6aaa77b4f7b7c11f96dc6a6a52e4006023b
longpv <pham2000long@gmail.com>
2021-08-26 12:13:42 +0700

    update

---
+++ QLTourism/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using QLTourism.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLTourism.Controllers
+{
+    public class LoginController : Controller
+    {
+        private TourismDB db = new TourismDB();
+        // GET: Admin/Login
+        [HttpGet]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
+        public ActionResult Index()
+        {
+            if (Session["idUser"] != null)
+            {
+                Session["Message"] = "Bạn đã đăng nhập!";
+                return RedirectToAction("Index", "Home");
+            }
+            return View("Index", "Home");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var data = db.Customers.Where(s => s.username.Equals(username) && s.password.Equals(password)).ToList();
+                    if (data.Count() > 0)
+                    {
+                        //add session
+                        Session["username"] = data.FirstOrDefault().username;
+                        Session["idUser"] = data.FirstOrDefault().id;
+                        Session["avatar"] = data.FirstOrDefault().avatar;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        Session["error"] = "Tài khoản hoặc mật khẩu không chính xác!";
+                        return RedirectToAction("Index");
+                    }
+                }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Lỗi không thể đăng nhập!" + ex.Message;
+                return View();
+            }
+        }
+
+        [HttpGet]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")]
+        public ActionResult Logout()
+        {
+            Session["username"] = null;
+            Session["idUser"] = null;
+            Session["avatar"] = null;
+            return RedirectToAction("Index");
+        }
+    }
+}
