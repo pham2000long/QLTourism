2e3a92c1b3eb8e50cb11905f6f93ff9e95970d86
HanaIroha <61019733+HanaIroha@users.noreply.github.com>
2021-08-27 18:16:22 +0700

    as

--- QLTourism/Controllers/CartController.cs
+++ QLTourism/Controllers/CartController.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using QLTourism.Models;
 
 namespace QLTourism.Controllers
 {
     public class CartController : Controller
     {
+        private TourismDB db = new TourismDB();
         // GET: Cart
         public ActionResult Index()
         {
+            if (Session["CartItem"] != null)
+            {
+                List<string[]> ds = new List<string[]>();
+                List<int> dsPkg = new List<int>();
+                string z = Session["CartItem"].ToString();
+                foreach (var item in z.Split('&'))
+                {
+                    var a = item.Split('|');
+                    ds.Add(a);
+                    dsPkg.Add(Int32.Parse(a[2]));
+                }
+                var pkg = db.Packages.Where(p => dsPkg.Contains(p.id));
+                ViewBag.dsSession = ds;
+                ViewBag.Pkg = pkg;
+            }
             return View();
         }
 
