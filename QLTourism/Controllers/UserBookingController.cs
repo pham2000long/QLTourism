30d13b4aa788439ad4d1866a09e9ab36abed34c0
HanaIroha <61019733+HanaIroha@users.noreply.github.com>
2021-08-28 19:05:21 +0700

    up

---
+++ QLTourism/Controllers/UserBookingController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using QLTourism.Models;
+
+namespace QLTourism.Controllers
+{
+    public class UserBookingController : Controller
+    {
+        private TourismDB db = new TourismDB();
+        // GET: UserBooking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
